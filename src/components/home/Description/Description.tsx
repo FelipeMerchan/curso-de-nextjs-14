8ce8cb3c1f900ded0642d28d09f889a4e47a0315
Felipe Merchan <felipemerchanfuquen@gmail.com>
2024-01-28 23:39:07 -0500

    14. Manejo y optimización de imágenes con Next Image

--- src/components/home/Description/Description.tsx
+++ src/components/home/Description/Description.tsx
@@ -1,11 +1,19 @@
 import React from 'react'
+import Image from 'next/image'
 
 import styles from './Description.module.sass'
 
 export const Description = () => {
   return (
     <section className={styles.Description}>
-      <img src="/images/description.jpeg" alt="Product marketplace" />
+      <Image
+        src="/images/description.jpeg"
+        alt="Product marketplace"
+        width="500"
+        height="300"
+        priority={false}
+        quality={100}
+      />
       <div>
         <h2>Description</h2>
         <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Exercitationem similique nostrum doloribus atque obcaecati ratione nisi eum nulla error, officia, fugit possimus voluptas neque tempore architecto iste consectetur placeat sed.</p>
