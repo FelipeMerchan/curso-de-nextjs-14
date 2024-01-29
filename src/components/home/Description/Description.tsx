35e71fa95c7109eb0024400c5e18d8d40bf81750
Felipe Merchan <felipemerchanfuquen@gmail.com>
2024-01-28 21:52:04 -0500

    Agregar estilos para la descripción

--- src/components/home/Description/Description.tsx
+++ src/components/home/Description/Description.tsx
@@ -1,11 +1,15 @@
 import React from 'react'
 
+import styles from './Description.module.sass'
+
 export const Description = () => {
   return (
-    <section>
+    <section className={styles.Description}>
       <img src="/images/description.jpeg" alt="Product marketplace" />
-      <h2>Description</h2>
-      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Exercitationem similique nostrum doloribus atque obcaecati ratione nisi eum nulla error, officia, fugit possimus voluptas neque tempore architecto iste consectetur placeat sed.</p>
+      <div>
+        <h2>Description</h2>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Exercitationem similique nostrum doloribus atque obcaecati ratione nisi eum nulla error, officia, fugit possimus voluptas neque tempore architecto iste consectetur placeat sed.</p>
+      </div>
     </section>
   )
 }
