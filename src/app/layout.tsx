0b44d933faeefc3cc42eeecd9d7260601eadf064
Felipe Merchan <felipemerchanfuquen@gmail.com>
2024-01-30 00:13:49 -0500

    16. Optimización de fuentes con Next.js

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,16 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Roboto } from 'next/font/google';
 
 import { Header } from "app/components/shared/Header";
 import { Footer } from "app/components/shared/Footer";
 import 'app/sass/globals.sass'
 
-const inter = Inter({ subsets: ["latin"] });
+const roboto = Roboto({
+  weight: ["100", "300", "500", "700"],
+  /* Caracteres especiales en la fuente que tienen algunas formas de escritura:
+  https://fonts.google.com/knowledge/glossary/subsetting */
+  subsets: ["latin"],
+});
 
 export const metadata: Metadata = {
   title: "Future world",
@@ -19,7 +24,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={roboto.className}>
         <Header />
         {children}
         <Footer />
