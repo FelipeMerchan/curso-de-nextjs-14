24936fbb4f430321723ec91b00164406f1a5312a
Felipe Merchan <felipemerchanfuquen@gmail.com>
2024-02-08 00:54:31 -0500

    20. Cómo obtener información de una API con Next.js

--- src/components/home/MainProducts/MainProducts.tsx
+++ src/components/home/MainProducts/MainProducts.tsx
@@ -1,8 +1,22 @@
-'use client'
 import React from 'react'
 
-export const MainProducts = () => {
-  console.log(process.env.NEXT_PUBLIC_SHOPIFY_HOSTNAME)
+const getProducts = async () => {
+  const response = await fetch(`${process.env.SHOPIFY_HOSTNAME}/admin/api/2023-10/products.json`, {
+    headers: {
+      'X-Shopify-Access-Token': process.env.SHOPIFY_API_KEY ?? "",
+    }
+  })
+
+  const data = await response.json()
+
+  return data
+}
+
+/* Los server component pueden ser asíncronos gracias a Next.js */
+export const MainProducts = async () => {
+  const products = await getProducts()
+  console.log(products)
+
   return (
     <div>MainProducts</div>
   )
