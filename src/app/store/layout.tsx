ea442231264248849c7b0b7c68cfeda2ef411012
Felipe Merchan <felipemerchanfuquen@gmail.com>
2024-02-24 00:46:17 -0500

    Agregar estilos de los chips

--- src/app/store/layout.tsx
+++ src/app/store/layout.tsx
@@ -1,20 +1,27 @@
 import Link from "next/link"
 
 import { getCollections } from "app/services/shopify/collections"
+import styles from './StoreLayout.module.sass'
 
 export default async function Layout({ children }: { children: React.ReactNode}) {
   const collections = await getCollections()
 
   return (
-    <main>
+    <main className={styles.StoreLayout}>
       <nav>
-        {
-          collections?.map((collection) => (
-            <Link key={collection.id} href={`store/${collection.handle}`}>
-              {collection.title}
-            </Link>
-          ))
-        }
+        <ul className={styles.StoreLayout__list}>
+          {
+            collections?.map((collection) => (
+              <Link
+                className={styles.StoreLayout__chip}
+                key={collection.id}
+                href={`store/${collection.handle}`}
+              >
+                {collection.title}
+              </Link>
+            ))
+          }
+        </ul>
       </nav>
       {children}
     </main>
