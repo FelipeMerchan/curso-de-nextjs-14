d34f833a8755ef3f503a8d59901bcd6fdeb722c8
Felipe Merchan <felipemerchanfuquen@gmail.com>
2024-02-25 17:43:06 -0500

    38. Mejorando SEO de una página en Next.js

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,4 +1,13 @@
 import { MainProducts } from "app/components/home/MainProducts";
+import { Metadata } from "next";
+
+/* Static Metadata: podemos agregar meta datos de SEO en páginas con rutas
+estáticas exportando la siguiente variable en el componente página: */
+export const metadata: Metadata = {
+  title: 'Future World',
+  description: 'Welcome to the future world, an e-commerce from other century',
+  keywords: ['e-commerce', 'future', 'technology'],
+}
 
 export default function Home() {
   return (
--- src/app/product/[handle]/page.tsx
+++ src/app/product/[handle]/page.tsx
@@ -8,6 +8,23 @@ interface ProductPageProps {
   }
 }
 
+/* Dynamic Metadata: cuando el contenido de la página es dinámico, debido a
+que la página es una ruta dinámica, podemos agregar los metadatos de la siguiente forma: */
+export async function generateMetadata({ searchParams }: ProductPageProps) {
+  const { id } = searchParams
+  const productList = await getProducts(id) as ProductType[]
+  const product = productList[0]
+
+  return {
+    title: product.title,
+    description: product.description,
+    keywords: product.tags,
+    openGraph: {
+      images: [product.image],
+    }
+  }
+}
+
 export default async function ProductPage({ searchParams }: ProductPageProps) {
   const { id } = searchParams
   const productList = await getProducts(id) as ProductType[]
