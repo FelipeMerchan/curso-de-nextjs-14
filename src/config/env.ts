01b706b44bf4c4750f58e9e6b0c0f7c499a47d80
Felipe Merchan <felipemerchanfuquen@gmail.com>
2024-02-25 18:30:32 -0500

    Agregar cliente de GraphQL

--- src/config/env.ts
+++ src/config/env.ts
@@ -5,4 +5,6 @@ export const env = {
   CACHE_TOKEN: process.env.CACHE_TOKEN,
   SHOPIFY_HOSTNAME: process.env.SHOPIFY_HOSTNAME ?? '',
   SHOPIFY_TOKEN: process.env.SHOPIFY_TOKEN ?? '',
+  SHOPIFY_GRAPHQL_ENDPOINT: process.env.SHOPIFY_GRAPHQL_ENDPOINT as string,
+  SHOPIFY_STOREFRONT_TOKEN: process.env.SHOPIFY_STOREFRONT_TOKEN as string,
 }
