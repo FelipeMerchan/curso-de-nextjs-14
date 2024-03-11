f3883ad69620a68c25e13fa95275225803aa4d48
Felipe Merchan <felipemerchanfuquen@gmail.com>
2024-03-11 00:47:59 -0500

    51. Implementación de un bot de ventas en una app Next.js

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -1,9 +1,16 @@
 import { Chat } from "app/components/chat/Chat";
+import { getProducts } from "app/services/shopify/products";
+import { createAgent } from "app/utils/openai/createAgent";
 
-export default function ChatPage() {
+export default async function ChatPage() {
+  const productList = await getProducts()
+  const productListTitles = productList?.map((product) => product.title)
+  const flatProductTitles = productListTitles?.join(', ')
+  const aiAgent = createAgent(flatProductTitles as string)
+
   return (
     <>
-      <Chat />
+      <Chat aiAgent={aiAgent} />
     </>
   )
 }
--- src/components/chat/Chat/Chat.tsx
+++ src/components/chat/Chat/Chat.tsx
@@ -1,14 +1,28 @@
 'use client';
  
 import { useChat } from 'ai/react';
+
+interface ChatProps {
+  aiAgent: string
+}
  
-export const Chat = () => {
-  const { messages, input, handleInputChange, handleSubmit } = useChat();
+export const Chat = ({ aiAgent }: ChatProps) => {
+  const { messages, input, handleInputChange, handleSubmit } = useChat({
+    /* Mensajes iniciales que le podemos asignar a nuestro chatbox
+    para que inicie con algún texto en específico: */
+    initialMessages: [
+      {
+        id: '1',
+        role: 'system',
+        content: aiAgent,
+      }
+    ]
+  });
  
   return (
     <div>
       <ul>
-        {messages.map((m, index) => (
+        {messages.filter((m) => m.role !== 'system').map((m, index) => (
           <li key={index}>
             {m.role === 'user' ? 'User: ' : 'AI: '}
             {m.content}
