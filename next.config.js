f427910a9e6fd43dc222ad02a3c72b86c7281643
Felipe Merchan <felipemerchanfuquen@gmail.com>
2024-03-16 17:10:14 -0500

    53. Análisis de bundle para Next.js 14

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 const path = require('path')
 
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+})
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   sassOptions: {
@@ -23,4 +27,4 @@ const nextConfig = {
   }
 };
 
-module.exports = nextConfig
+module.exports = withBundleAnalyzer(nextConfig)
--- src/utils/auth/createAccessToken.ts
+++ src/utils/auth/createAccessToken.ts
@@ -5,7 +5,14 @@ import { cookies } from 'next/headers'
 export const createAccessToken = async (email: string, password: string) => {
   const cookiesStore = cookies()
   const graphQLClient = GraphQLClientSingleton.getInstance().getClient()
-  const { customerAccessTokenCreate } = await graphQLClient.request(customerAccessTokenCreateMutation, {
+  const { customerAccessTokenCreate }: {
+    customerAccessTokenCreate: {
+      customerAccessToken: {
+        accessToken: string;
+        expiresAt: string;
+      }
+    }
+  } = await graphQLClient.request(customerAccessTokenCreateMutation, {
     email,
     password,
   })
